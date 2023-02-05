031426deae1fbab8a6e031e3e865837cc0f1082f
hourglass2020 <pouria.fm15@gmail.com>
2023-02-05 12:01:13 +0330

    use formik component and formik's clean code

--- src/components/Contacts/AddContact.jsx
+++ src/components/Contacts/AddContact.jsx
@@ -1,4 +1,4 @@
-import { useFormik } from "formik";
+import { Formik } from "formik";
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 
@@ -13,25 +13,8 @@ const AddContact = () => {
     contact,
     onContactChange: setContactInfo,
     groups,
-    // errors,
-    // createContact: createContactForm,
     createContact,
   } = useContext(ContactContext);
-
-  const formik = useFormik({
-    initialValues: {
-      fullname: "",
-      photo: "",
-      mobile: "",
-      email: "",
-      job: "",
-      group: "",
-    },
-    validationSchema: contactSchema,
-    onSubmit: (values) => {
-      createContact(values);
-    },
-  });
 
   return (
     <>
@@ -65,153 +48,127 @@ const AddContact = () => {
               <hr style={{ backgroundColor: GREEN }} />
               <div className="row mt-5">
                 <div className="col-md-4">
-                  {/* {
-                    errors?.map((error, index) => (
-                      <p className="alert alert-danger" dir="ltr" key={index}>{error.message}</p>
-                    ))
-                  } */}
-                  <form onSubmit={formik.handleSubmit}>
-                    <div className="mb-2">
-                      <input
-                        name="fullname"
-                        type="text"
-                        id="fullname"
-                        // value={contact.fullname}
-                        // onChange={setContactInfo}
-                        value={formik.values.fullname}
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-                        className="form-control"
-                        placeholder="نام و نام خانوادگی"
-                      //required={true}
-                      />
-                      {formik.touched.fullname && formik.errors.fullname ? (
-                        <p className="text-danger">{formik.errors.fullname}</p>
-                      ) : null}
-                    </div>
-                    <div className="mb-2">
-                      <input
-                        name="photo"
-                        type="text"
-                        id="photo"
-                        // value={contact.photo}
-                        // onChange={setContactInfo}
-                        value={formik.values.photo}
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
 
-                        className="form-control"
-                        //required={true}
-                        placeholder="آدرس تصویر"
-                      />
-                      {formik.touched.photo && formik.errors.photo ? (
-                        <p className="text-danger">{formik.errors.photo}</p>
-                      ) : null}
-                    </div>
-                    <div className="mb-2">
-                      <input
-                        name="mobile"
-                        type="number"
-                        id="mobile"
-                        // value={contact.mobile}
-                        // onChange={setContactInfo}
-                        value={formik.values.mobile}
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
+                  <Formik
+                    initialValues={{
+                      fullname: "",
+                      photo: "",
+                      mobile: "",
+                      email: "",
+                      job: "",
+                      group: "",
+                    }}
+                    validationSchema={contactSchema}
+                    onSubmit={(values) => {
+                      createContact(values);
+                    }}>
 
-                        className="form-control"
-                        //required={true}
-                        placeholder="شماره موبایل"
-                      />
-                      {formik.touched.mobile && formik.errors.mobile ? (
-                        <p className="text-danger">{formik.errors.mobile}</p>
-                      ) : null}
-                    </div>
-                    <div className="mb-2">
-                      <input
-                        type="email"
-                        name="email"
-                        id="email"
-                        // value={contact.email}
-                        // onChange={setContactInfo}
-                        value={formik.values.email}
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-
-                        className="form-control"
-                        //required={true}
-                        placeholder="آدرس ایمیل"
-                      />
-                      {formik.touched.email && formik.errors.email ? (
-                        <p className="text-danger">{formik.errors.email}</p>
-                      ) : null}
-                    </div>
-                    <div className="mb-2">
-                      <input
-                        type="text"
-                        name="job"
-                        id="job"
-                        // value={contact.job}
-                        // onChange={setContactInfo}
-                        value={formik.values.job}
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-
-                        className="form-control"
-                        //required={true}
-                        placeholder="شغل"
-                      />
-                      {formik.touched.job && formik.errors.job ? (
-                        <p className="text-danger">{formik.errors.job}</p>
-                      ) : null}
-                    </div>
-                    <div className="mb-2">
-                      <select
-                        id="group"
-                        name="group"
-                        // value={contact.group}
-                        // onChange={setContactInfo}
-                        value={formik.values.group}
-                        onChange={formik.handleChange}
-                        onBlur={formik.handleBlur}
-
-                        //required={true}
-                        className="form-control"
-                      >
-                        <option value="">انتخاب گروه</option>
-                        {groups.length > 0 &&
-                          groups.map((group) => (
-                            <option key={group.id} value={group.id}>
-                              {group.name}
-                            </option>
-                          ))}
-                      </select>
-                      {formik.touched.group && formik.errors.group ? (
-                        <p className="text-danger">{formik.errors.group}</p>
-                      ) : null}
-                    </div>
-                    <div className="mx-2">
-                      <input
-                        type="submit"
-                        className="btn"
-                        style={{ backgroundColor: PURPLE }}
-                        value="ساخت مخاطب"
-                      />
-                      <Link
-                        to={"/contacts"}
-                        className="btn mx-2"
-                        style={{ backgroundColor: COMMENT }}
-                      >
-                        انصراف
-                      </Link>
-                    </div>
-                  </form>
+                    <form onSubmit={formik.handleSubmit}>
+                      <div className="mb-2">
+                        <input
+                          id="fullname"
+                          type="text"
+                          // value={formik.values.fullname}
+                          // onChange={formik.handleChange}
+                          // onBlur={formik.handleBlur}
+                          {...formik.getFieldProps('fullname')}
+                          className="form-control"
+                          placeholder="نام و نام خانوادگی"
+                        />
+                        {formik.touched.fullname && formik.errors.fullname ? (
+                          <p className="text-danger">{formik.errors.fullname}</p>
+                        ) : null}
+                      </div>
+                      <div className="mb-2">
+                        <input
+                          id="photo"
+                          type="text"
+                          {...formik.getFieldProps('photo')}
+                          className="form-control"
+                          placeholder="آدرس تصویر"
+                        />
+                        {formik.touched.photo && formik.errors.photo ? (
+                          <p className="text-danger">{formik.errors.photo}</p>
+                        ) : null}
+                      </div>
+                      <div className="mb-2">
+                        <input
+                          type="number"
+                          id="mobile"
+                          {...formik.getFieldProps('mobile')}
+                          className="form-control"
+                          placeholder="شماره موبایل"
+                        />
+                        {formik.touched.mobile && formik.errors.mobile ? (
+                          <p className="text-danger">{formik.errors.mobile}</p>
+                        ) : null}
+                      </div>
+                      <div className="mb-2">
+                        <input
+                          id="email"
+                          type="email"
+                          {...formik.getFieldProps('email')}
+                          className="form-control"
+                          placeholder="آدرس ایمیل"
+                        />
+                        {formik.touched.email && formik.errors.email ? (
+                          <p className="text-danger">{formik.errors.email}</p>
+                        ) : null}
+                      </div>
+                      <div className="mb-2">
+                        <input
+                          id="job"
+                          type="text"
+                          {...formik.getFieldProps('job')}
+                          className="form-control"
+                          placeholder="شغل"
+                        />
+                        {formik.touched.job && formik.errors.job ? (
+                          <p className="text-danger">{formik.errors.job}</p>
+                        ) : null}
+                      </div>
+                      <div className="mb-2">
+                        <select
+                          id="group"
+                          {...formik.getFieldProps('group')}
+                          className="form-control"
+                        >
+                          <option value="">انتخاب گروه</option>
+                          {groups.length > 0 &&
+                            groups.map((group) => (
+                              <option key={group.id} value={group.id}>
+                                {group.name}
+                              </option>
+                            ))}
+                        </select>
+                        {formik.touched.group && formik.errors.group ? (
+                          <p className="text-danger">{formik.errors.group}</p>
+                        ) : null}
+                      </div>
+                      <div className="mx-2">
+                        <input
+                          type="submit"
+                          className="btn"
+                          style={{ backgroundColor: PURPLE }}
+                          value="ساخت مخاطب"
+                        />
+                        <Link
+                          to={"/contacts"}
+                          className="btn mx-2"
+                          style={{ backgroundColor: COMMENT }}
+                        >
+                          انصراف
+                        </Link>
+                      </div>
+                    </form>
+                  </Formik>
                 </div>
               </div>
             </div>
           </section>
         </>
-      )}
+      )
+      }
     </>
   );
 };
